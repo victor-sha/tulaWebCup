533696bcdec5d8ad8f407eaafdd6dc6a82c05aed
victor-sha <kassini_90@mail.ru>
2019-04-08 12:51:16 +0300

    setup server & build folder

---
+++ frontend/build/precache-manifest.240cb470c03bfab471d8feda81452f5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2d19fc572097c1bf9b7b",
+    "url": "/static/js/main.e412f6a1.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "2d19fc572097c1bf9b7b",
+    "url": "/static/css/main.913019ae.chunk.css"
+  },
+  {
+    "revision": "38cd210a1f0c36630245255aed2526a7",
+    "url": "/index.html"
+  }
+];
--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
-const express = require('express');
+const express = require("express");
+const favicon = require("express-favicon");
+const path = require("path");
+const port = process.env.PORT || 4000;
+const build_path = "frontend/build";
 
 const app = express();
+app.use(favicon(__dirname + `${build_path}/favicon.ico`));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, build_path)));
 
-const router = express.Router();
+app.get("/ping", function(req, res) {
+  return res.send("pong");
+});
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, build_path, "index.html"));
+});
 
-app.get('/main', (req, res) => {
-    return res.send('hello')
-})
+app.get("/main", (req, res) => {
+  return res.send("hello");
+});
 
-app.listen(3000, () => console.log('server is running'))
+app.listen(port, () => console.log("server is running"));
