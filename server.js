4ada1940c9417957e780f05aa4ab7cea577c362b
victor-sha <kassini_90@mail.ru>
2019-04-08 13:26:17 +0300

    Refactor server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const favicon = require("express-favicon");
 const path = require("path");
 const port = process.env.PORT || 4000;
-const build_path = "frontend/build";
+const build_path = "/frontend/build";
 
 const app = express();
 app.use(favicon(__dirname + `${build_path}/favicon.ico`));
@@ -20,4 +20,4 @@ app.get("/main", (req, res) => {
   return res.send("hello");
 });
 
-app.listen(port, () => console.log("server is running"));
+app.listen(port, () => console.log("server is running on port " + port));
